ffd321aee4d520773a3245a53188b378c0cb013f
James Thompson <james@plainprograms.com>
2013-01-29 11:54:57 -0700

    Fixed hash format not compatible with Ruby 1.8.

--- spec/classes/torquebox_spec.rb
+++ spec/classes/torquebox_spec.rb
@@ -10,19 +10,19 @@ describe 'torquebox' do
     it { should contain_package('openjdk-6-jre-headless').with_ensure('present') }
 
     describe 'set to true' do
-      let(:params) { { use_latest: true } }
+      let(:params) { { :use_latest => true } }
 
       it { should contain_package('openjdk-6-jre-headless').with_ensure('latest') }
     end
 
     describe 'set to false' do
-      let(:params) { { use_latest: false } }
+      let(:params) { { :use_latest => false } }
 
       it { should contain_package('openjdk-6-jre-headless').with_ensure('present') }
     end
 
     describe 'set to a non-boolean' do
-      let(:params) { { use_latest: 'string' } }
+      let(:params) { { :use_latest => 'string' } }
 
       it { expect{ subject }.to raise_error(/^The use_latest parameter must be either true or false\./) }
     end
